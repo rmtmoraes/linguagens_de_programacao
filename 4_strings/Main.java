4d3367289ed8e1ca5dae2848c72c840cd0d07834
rmtmoraes <rmt_moraes@hotmail.com>
2026-08-26 22:29:22 -0300

    Exercicios

--- 4_strings/Main.java
+++ 4_strings/Main.java
@@ -1,12 +1,43 @@
 
 public class Main {
     public static void main(String[] args) {
-     String saudacao = "Hello Word!";
-     System.out.println(saudacao.length()); //contagem de caracteres
-     System.out.println(saudacao.toUpperCase()); //letra maiuscula  
-     System.out.println(saudacao.toLowerCase()); //letra minuscula
-     System.out.println(saudacao.indexOf("W")); //busca posicao da letra   
-     System.out.println(saudacao.charAt(6)); //busca letra na posicao   
+ //    String saudacao = "Hello Word!";
+ //    System.out.println(saudacao.length()); //contagem de caracteres
+ //    System.out.println(saudacao.toUpperCase()); //letra maiuscula  
+ //    System.out.println(saudacao.toLowerCase()); //letra minuscula
+ //    System.out.println(saudacao.indexOf("W")); //busca posicao da letra   
+ //    System.out.println(saudacao.charAt(6)); //busca letra na posicao   
+ //   }
+
+        //Estamos usando o padrão snake_case
+        String aluno_1 = "Miranata";
+        String aluno_2 = "Aeronauta";
+        System.out.println(aluno_1.equals(aluno_2)); // compara
+
+        String mensagem = " Hello Word ";
+        System.out.println("-" + mensagem.trim() + "-"); //remove espaços e inclui traço
+        
+        String nome = "Savalo";
+        String sobrenome = "Horse";
+        System.out.println(nome + " " + sobrenome);
+
+        String aluno_3 = "Pombinha da Paz Silva";
+        System.out.println(aluno_3.contains("Paz")); //contém algo na string
+
+        String aluno_4 = "";
+        System.out.println(aluno_4.isEmpty()); //valida se string está vazia
+
+        String frutas = String.join("-", "Laranja", "Banana"); //junta as strings com "-"
+        System.out.println(frutas);
+
+        String nome_completo = "Del Lokom Pirom";
+        System.out.println(nome_completo.replace("m", "n")); //substitui algo na string
+
+        String texto = "Boa noite!";
+        System.out.println(texto.substring(4, 9));
+
+        String frase = "Repetição com \"Correção\" leva a \"Perfeição\""; // insere barra dentro da string (caracteres especiais)
+        System.out.println(frase);
+
     }
-    
 }
---
+++ 5_funcoes_matematicas/Main.java
@@ -0,0 +1,39 @@
+public class Main {
+    public static void main(String[] args) {
+    //    System.out.println(Math.max(5, 10)); //busca valor maximo
+    //    System.out.println(Math.min(5, 10)); //busca valor minimo
+    //    System.out.println(Math.sqrt(64)); //raiz quadrada
+    //    System.out.println(Math.abs(-4.7)); //deixa como numero positivo
+    //    System.out.println(Math.pow(2, 8)); //2^8 = 256 achar potencia
+    //    System.out.println(Math.round(4.6)); //arredondar tanto para cima quanto para baixo - automatico
+    //    System.out.println(Math.ceil(4.1)); //arredondar para cima
+    //    System.out.println(Math.floor(4.1)); //arredondar para baixo
+    //    System.out.println(Math.random()); //gera um numero aleatório
+
+    //    boolean luz = false;                                //exercicio: script para acender ou apagar a luz
+    //    if (luz) {
+    //        System.out.println("Luz esta acessa");
+    //    }
+    //    else {
+    //        System.out.println("Luz está apagada!");
+    //   }
+    //   }
+    
+    
+    //    boolean luz_1 = true;
+    //    String resultado = (luz_1) ? "Luz está acessa!" : "Luz está apagada!";  // IFternário: não ha necessidade do else
+    //         System.out.println(resultado);
+   
+    int idade = 50;
+        if (idade <= 12)
+            System.out.println("Criança");
+         else if (idade >= 12 && idade <18) {
+                System.out.println("Adolescente");
+        } else if (idade >= 18 && idade <60); {
+                System.out.println("Adulto");
+            }    else {
+                        System.out.println("Idoso");
+        }
+    }
+
+        
